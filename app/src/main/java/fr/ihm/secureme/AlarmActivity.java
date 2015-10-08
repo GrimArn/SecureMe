89c0c2fa27cc82c485246e1acd00d9bc620efea6
Nonau <arnaud.grima@gmail.com>
2015-10-08 07:28:37 +0200

    déclenchement alarme quand arrivée d'un message

---
+++ app/src/main/java/fr/ihm/secureme/AlarmActivity.java
@@ -0,0 +1,121 @@
+package fr.ihm.secureme;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class AlarmActivity extends ActionBarActivity {
+
+    RelativeLayout rl;
+    Timer t1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+        rl = (RelativeLayout) findViewById(R.id.mainlayout);
+        t1 = new Timer();
+
+        initLight();
+    }
+
+    private void initLight() {
+        lightRed();
+    }
+
+    private void lightRed() {
+        t1.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        rl.setBackgroundColor(getResources().getColor(R.color.red));
+                        lightBlue();
+                    }
+                });
+
+            }
+        }, 50);
+
+    }
+    private void lightBlue() {
+        t1.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        rl.setBackgroundColor(getResources().getColor(R.color.blue));
+                        lightGreen();
+                    }
+                });
+
+            }
+        }, 50);
+
+    }
+
+    private void lightGreen() {
+        t1.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        rl.setBackgroundColor(getResources().getColor(R.color.green));
+                        lightYellow();
+                    }
+                });
+
+            }
+        }, 50);
+
+    }
+
+    private void lightYellow() {
+        t1.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        rl.setBackgroundColor(getResources().getColor(R.color.yellow));
+                        lightRed();
+                    }
+                });
+
+
+            }
+        }, 50);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_alarm, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/fr/ihm/secureme/SmsBroadcastReceiver.java
@@ -0,0 +1,50 @@
+package fr.ihm.secureme;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+/**
+ * Created by nonau on 06/10/15.
+ */
+public class SmsBroadcastReceiver extends BroadcastReceiver{
+
+    final SmsManager smsManager = SmsManager.getDefault();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final Bundle bundle = intent.getExtras();
+        Log.v("TAG", "coucou");
+        try {
+            if (bundle != null) {
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+
+                for (int i = 0; i < pdusObj.length; ++i) {
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[])pdusObj[i]);
+                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+                    String senderNum = phoneNumber;
+                    String message = currentMessage.getDisplayMessageBody();
+
+                    Toast toast = Toast.makeText(context, "Message reçu de : " + senderNum + " message : " + message, Toast.LENGTH_LONG);
+                    toast.show();
+
+                    Intent intent1 = new Intent();
+                    intent1.setClassName("fr.ihm.secureme", "fr.ihm.secureme.AlarmActivity");
+                    intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent1);
+
+                }
+            }
+        } catch (Exception e) {
+            Log.e("Sms", "Ex" + e);
+        }
+    }
+}
